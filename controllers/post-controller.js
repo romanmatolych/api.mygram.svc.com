7360d203632b7c9b22daa9471216e48dc9aa0c02
Roman Matolych <romanmatolych@gmail.com>
2019-02-21 18:30:12 +0200

    Protect Post calls with jwt

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const {validationResult} = require('express-validator/check');
 const debug = require('debug')('api.mygram.svc.com:post-controller');
+const {Types: {ObjectId}} = require('mongoose');
 
 const Post = require('../models/post');
 
@@ -10,15 +11,31 @@ const Post = require('../models/post');
  */
 class PostController {
 
-    static getPost(req, res, next) {
-        Post.findById(res.locals.postId, function(err, post) {
+    /**
+     * Sorts all blogs by date in ascending order to 
+     * get an id of the blog at the index from the param
+     * and passes it to the next middleware
+     */
+    static findPost(req, res, next, postInd) {
+        Post.find({blogId: req.params.blogId}).sort({createdAt: 1}).exec(function(err, blogPosts) {
             if (err) return next(err);
-            if (post === null) return next(createError(404, 'Post Not Found'));
     
-            debug(`Get post ${post._id}`);
-
-            res.json({post});
+            const i = parseInt(postInd) - 1;
+            if (!Number.isInteger(i) || i < 1) return next(createError(400));
+            if (typeof blogPosts[i] === 'undefined') {
+                debug(`Cannot find post at index ${postInd}`);
+                next(createError(404, 'Post Not Found'));
+            } else {
+                debug(`Found post at index ${i}(${postInd})`);
+                res.locals.post = blogPosts[i];
+                next();
+            } 
         });
+    }
+
+    static getPost(req, res, next) {
+        const post = res.locals.post;
+        res.json({post});
     }
 
     static createNewPost(req, res, next) {
@@ -27,67 +44,66 @@ class PostController {
         
         // New post object to be created
         const post = {
-            blogId: res.locals.blogId,
+            blogId: req.params.blogId,
             desc: req.body.desc,
             imgUrl: req.body.imgUrl
         };
 
-        if (!errors.isEmpty()) {
-            debug('Post creation validation errors: %j', errors.array());
+        const payload = res.locals.payload,
+            blog = res.locals.blog;
+        if (payload && payload.userId == blog.userId) {
+            // Validate blogId
+            if (!ObjectId.isValid(post.blogId)) return next(createError(400));
+            if (!errors.isEmpty()) {
+                debug('Post creation validation errors: %j', errors.array());
+                return res.status(400).json({errors: errors.array(), post});
+            }
 
-            return res.status(400).json({errors: errors.array(), post});
-        }
-
-        Post.create(post, function(err, newPost) {
-            if (err) return next(err);
-
-            // Calculate index of a new post for its URL
-            newPost.getIndex(function(err, i) {
+            Post.create(post, function(err, newPost) {
                 if (err) return next(err);
-                if (i === -1) return next(createError(500));
 
-                debug('Created new post at %d index, %O', i, newPost);
+                // Calculate index of a new post for its URL
+                newPost.getIndex(function(err, i) {
+                    if (err) return next(err);
+                    if (i === -1) return next(createError(500));
 
-                res.redirect(201, newPost.baseUrl + `/${i + 1}`);
+                    debug('Created new post at %d index, %O', i, newPost);
+                    res.redirect(201, newPost.baseUrl + `/${i + 1}`);
+                });
             });
-        });
+        } else {
+            next(createError(403));
+        }
     }
 
     static updatePost(req, res, next) {
         // Extract validation errors from a request
         const errors = validationResult(req);
-
-        // Update object
-        const post = {
-            _id: res.locals.postId,
-            desc: req.body.desc,
-            imgUrl: req.body.imgUrl
-        };
-
         if (!errors.isEmpty()) {
             debug('Post update validation errors: %j', errors.array());
-
-            return res.status(400).json({errors: errors.array(), post});
+            return res.status(400).json({errors: errors.array(), post: req.body});
         }
 
-        Post.findByIdAndUpdate(res.locals.postId, post, function (err, origPost) {
+        // Update object
+        const post = res.locals.post; 
+        post.desc = req.body.desc;
+        post.imgUrl = req.body.imgUrl;
+        post.save(function(err, savedPost) {
             if (err) return next(err);
 
-            debug(`Updated post ${origPost._id}`);
-
+            debug(`Updated post ${savedPost._id}`);
             res.redirect(200, req.originalUrl);
         });
     }
 
     // Sends the deleted post
     static deletePost(req, res, next) {
-        Post.findByIdAndRemove(res.locals.postId, function(err, foundPost) {
+        const post = res.locals.post;
+        post.remove(function(err, removedPost) {
             if (err) return next(err);
-            if (foundPost === null) return next(createError(404, 'Post Not Found'));
     
-            debug(`Deleted post ${foundPost._id}`);
-
-            res.json({post: foundPost});
+            debug(`Deleted post ${removedPost._id}`);
+            res.json({post: removedPost});
         });
     }
 
